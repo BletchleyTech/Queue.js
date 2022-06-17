3d14c7f1701fd1b02de3b737d1c86fc90bc7bfde
Jorge Gallego <gallegojorge908@gmail.com>
2022-06-17 11:47:30 -0500

    Queue.js compiled from src/Queue.ts

--- Queue.js
+++ Queue.js
@@ -1,34 +1,44 @@
-module.exports = class Queue {
+export default class Queue {
     #queue;
-    constructor() {
-        this.#queue = [];
+    #type;
+    constructor(queue) {
+        this.#queue = queue ? queue.#queue : [];
+        this.#type = queue ? queue.#type : "";
     }
     get size() {
         return this.#queue.length;
     }
     get isEmpty() {
-        return this.#queue.length === 0;
+        return this.size === 0;
     }
     get front() {
         return this.#getFront();
     }
     get queue() {
-        return this.#queue.join(" - ");
+        return this.#toString();
     }
     enqueue(element) {
+        if (this.isEmpty && !this.#type)
+            this.#type = typeof element;
+        if (typeof element !== this.#type)
+            throw new Error("Invalid type");
         this.#queue.push(element);
     }
     enqueueMany(...elements) {
-        elements.flat().forEach(element => this.enqueue(element));
+        elements.forEach(element => this.enqueue(element));
     }
     dequeue() {
-        if (this.isEmpty)
-            return "Underflow";
-        return this.#queue.shift();
+        const item = this.#queue.shift();
+        if (item === undefined)
+            throw new Error("Underflow");
+        return item;
+    }
+    #toString() {
+        return this.#queue.join(" - ");
     }
     #getFront() {
         if (this.isEmpty)
-            return "No elements in Queue";
+            throw new Error("Underflow");
         return this.#queue[0];
     }
 }
